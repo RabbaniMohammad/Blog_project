f6ac41f15741b9c8705bbdc335065c9306f68237
Mohammad Rabbani <mohammad.rabbani@tigeranalytics.local>
2022-05-11 13:16:57 +0530

    updated

---
+++ src/components/Category.jsx
@@ -0,0 +1,68 @@
+export default function Category() {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="imgContainer">
+            <img
+              src="https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_960_720.png"
+              alt="img not found"
+            />
+          </div>
+          <h3>Which Programming Language should i learn in 2022</h3>
+          <p>Programming</p>
+          <p>
+            <strong>Joel Falconer</strong>
+            <span>May 04</span>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet soluta
+            laborum non dolore unde praesentium dolores a fuga incidunt corporis
+            deserunt nostrum placeat odio architecto aliquam officia, expedita
+            quo dicta.
+          </p>
+        </div>
+        <div className="container">
+          <div className="imgContainer">
+            <img
+              src="https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_960_720.png"
+              alt=""
+            />
+          </div>
+          <h3>Which Programming Language should i learn in 2022</h3>
+          <p>Programming</p>
+          <p>
+            <strong>Joel Falconer</strong>
+            <span>May 04</span>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet soluta
+            laborum non dolore unde praesentium dolores a fuga incidunt corporis
+            deserunt nostrum placeat odio architecto aliquam officia, expedita
+            quo dicta.
+          </p>
+        </div>
+        <div className="container">
+          <div className="imgContainer">
+            <img
+              src="https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_960_720.png"
+              alt=""
+            />
+          </div>
+          <h3>Which Programming Language should i learn in 2022</h3>
+          <p>Programming</p>
+          <p>
+            <strong>Joel Falconer</strong>
+            <span>May 04</span>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet soluta
+            laborum non dolore unde praesentium dolores a fuga incidunt corporis
+            deserunt nostrum placeat odio architecto aliquam officia, expedita
+            quo dicta.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
