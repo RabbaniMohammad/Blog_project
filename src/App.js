2035863eed18b48ed0e7dd6f4c2718d496896be9
Mohammad Rabbani <mohammad.rabbani@tigeranalytics.local>
2022-05-11 13:15:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { About } from "./components/About";
+import Category from "./components/Category";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/category" element={<Category />}></Route>
         <Route exact path="/about" element={<About />}></Route>
       </Routes>
     </BrowserRouter>
