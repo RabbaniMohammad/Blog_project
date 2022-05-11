18fa69ae8c49993194902aee9f4b8c025ef2854d
Mohammad Rabbani <mohammad.rabbani@tigeranalytics.local>
2022-05-11 10:55:24 +0530

    done

---
+++ src/components/About.js
@@ -0,0 +1,83 @@
+export const About = () => {
+  return (
+    <>
+      <header>
+        <h1>React Router Application</h1>
+        <p>
+          Create an application in which user can view articles on different
+          topics. The application should have the following links
+        </p>
+      </header>
+      <section>
+        <nav>
+          <ul>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Articles</a>
+            </li>
+            <li>
+              <a>Categories</a>
+            </li>
+            <li>
+              <a>Subscribe</a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <h2>
+            <strong>Description:-</strong>
+          </h2>
+          <ul>
+            <li>
+              <strong>About:&nbsp;</strong>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi iste aliquid fugiat quasi asperiores, natus
+                consequatur! Aperiam consequatur ab inventore?
+              </span>
+            </li>
+            <li>
+              <strong>Article:&nbsp;</strong>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi iste aliquid fugiat quasi asperiores, natus
+                consequatur! Aperiam consequatur ab inventore?
+              </span>
+            </li>
+            <li>
+              <strong>Categories:&nbsp;</strong>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi iste aliquid fugiat quasi asperiores, natus
+                consequatur! Aperiam consequatur ab inventore?
+              </span>
+            </li>
+            <li>
+              <strong>Subscribe:&nbsp;</strong>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi iste aliquid fugiat quasi asperiores, natus
+                consequatur! Aperiam consequatur ab inventore?
+              </span>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <footer>
+        <strong>Footr section:-</strong>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+          autem error aliquid voluptatum odit magnam molestiae magni porro
+          dolore eum. <strong>back and next</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel vitae
+          repudiandae recusandae. Culpa officia at nisi laborum, dicta aliquam
+          eaque.
+        </p>
+      </footer>
+    </>
+  );
+};
