478dbce187fd2c078c32fa237279e01c05ba9fb8
Mohammad Rabbani <mohammad.rabbani@tigeranalytics.local>
2022-05-11 11:56:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { About } from "./components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <About />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
